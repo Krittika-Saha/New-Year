fe2b26a8a75c7f56320e890a3366fe34f5c45c71
Krittika-Saha <krittika.saha.dev@gmail.com>
2020-12-31 23:17:22 +0530

    Reduced code

--- public/js/index.js
+++ public/js/index.js
@@ -1,36 +1,3 @@
-
-// let hours = ''
-// let daysItem = document.querySelector("#days");
-// let hoursItem = document.querySelector("#hours");
-// let minItem = document.querySelector("#mins");
-// let secItem = document.querySelector("#secs");
-
-
-// let countDown = () => {
-//   let futureDate = new Date("1 Jan 2021 00:00:00");
-//   let currentDate = new Date();
-//   let myDate = futureDate - currentDate;
-
-//   let days = "0"+Math.floor(myDate / 1000 / 60 / 60 / 24);
-
-//   hours = "0"+Math.floor(myDate / 1000 / 60 / 60) % 24;
-
-//   let min = Math.floor(myDate / 1000 / 60) % 60;
-
-//   let sec = Math.floor(myDate / 1000) % 60;
-
-//   daysItem.innerHTML = days;
-//   hoursItem.innerHTML = hours;
-//   minItem.innerHTML = min;
-//   secItem.innerHTML = sec;
-// }
-
-// countDown()
-// if (min != 0 ||sec != 0){
-//   setInterval(countDown, 1000)
-// } else {
-//   document.querySelector("#time").innerHTML = '<h1>Happy New Year!</h1>'
-// }
 // Set the date we're counting down to
 var countDownDate = new Date("Jan 1, 2021 00:00:00").getTime();
 
@@ -44,8 +11,8 @@ var x = setInterval(function() {
   var distance = countDownDate - now;
     
   // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var days = "0"+Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = "0"+Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
